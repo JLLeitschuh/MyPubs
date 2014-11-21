8d2688291fba69924f82091a71df104abf7d4482
jkreft-usgs <jkreft-usgs@users.noreply.github.com>
2014-11-21 11:05:09 -0600

    italic as i instead of em

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/bibliodata/bibliodata.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/bibliodata/bibliodata.js
@@ -179,6 +179,9 @@
 				$scope.docAbstractEditorOptions = {
 					menubar: false,
 					plugins : 'code link paste',
+					formats: {
+    						italic: {inline: 'i'}
+  						},
 					browser_spellcheck : true,
 					toolbar : 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | subscript superscript | link | code'
 				};
