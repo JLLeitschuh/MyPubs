17188cee673368edad098e8245c05af7cc297a3c
drsteini <drsteini@usgs.gov>
2015-01-08 14:32:54 -0600

    PUBSTWO-1135

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/spn/spn.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/spn/spn.js
@@ -8,6 +8,8 @@ angular.module('pw.spn', ['pw.lookups'])
 			//@mbucknell recommends binding ui-select2 ng-model targets to variables
 			//on the local-most scope and propagating changes back up to pubData via watches
 			$scope.localPscId = $scope.pubData.publishingServiceCenter.id;
+			$scope.localIsPartOfId = $scope.pubData.isPartOf.id;
+			$scope.localSupersededById = $scope.pubData.supersededBy.id;
 
 			// This method and the watches on the select2 local values gets around an issue where you
 			// have to initialize the select2 with an id value, but then it puts an object on the associated
@@ -23,14 +25,59 @@ angular.module('pw.spn', ['pw.lookups'])
 					return {id: obj.id, text : obj.text};
 				}
 			};
-			$scope.$watch('localPscId', function (value) {
-				$scope.pubData.publishingServiceCenter = updateObject($scope.pubData.publishingServiceCenter, value);
+
+			$scope.$watch('localPscId', function (newVal, oldVal) {
+				if (newVal !== oldVal) {
+					$scope.pubData.publishingServiceCenter.id = newVal;
+				}
+			});
+			$scope.$watch('localIsPartOfId', function (newVal, oldVal) {
+				if (newVal !== oldVal) {
+					$scope.pubData.isPartOf = updateObject($scope.pubData.isPartOf, newVal);
+				}
+			});
+			$scope.$watch('localSupersededById', function (newVal, oldVal) {
+				if (newVal !== oldVal) {
+					$scope.pubData.supersededBy = updateObject($scope.pubData.supersededBy, newVal);
+				}
 			});
 
 			LookupFetcher.promise('publishingServiceCenters').then(function (response) {
 				$scope.pscOptions = response.data;
 			});
-
+			
+			$scope.isPartOfOptions = {
+					query: function (options) {
+						if (!options.term && $scope.pubData && $scope.pubData.isPartOf && $scope.pubData.isPartOf.indexId) {
+							options.term = $scope.pubData.isPartOf.indexId.slice(0, -2);
+						}
+						LookupFetcher.promise('publications', {text : options.term}).then(function(response) {
+							options.callback({results : response.data});
+		                });
+					},
+					initSelection: function (element, callback) {
+						callback($scope.pubData.isPartOf);
+					},
+					placeholder: 'Select the Is Part of Publication',
+					allowClear: true
+				};
+			
+			$scope.supersededByOptions = {
+					query: function (options) {
+						if (!options.term && $scope.pubData && $scope.pubData.supersededBy && $scope.pubData.supersededBy.indexId) {
+							options.term = $scope.pubData.supersededBy.indexId.slice(0, -2);
+						}
+						LookupFetcher.promise('publications', {text : options.term}).then(function(response) {
+							options.callback({results : response.data});
+		                });
+					},
+					initSelection: function (element, callback) {
+						callback($scope.pubData.supersededBy);
+					},
+					placeholder: 'Select the Superseded By Publication',
+					allowClear: true
+				};
+			
 			$scope.contactsEditorOptions = {
 					menubar: false,
 					plugins : 'code link paste',
