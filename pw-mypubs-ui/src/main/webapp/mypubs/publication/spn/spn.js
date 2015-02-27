19dba333c6a594f69c81918c3dffae33977923c2
jkreft-usgs <jkreft-usgs@users.noreply.github.com>
2015-02-27 16:20:42 -0600

    remove custom tag set
    The custom tag set was messing up link creation

--- pw-mypubs-ui/src/main/webapp/mypubs/publication/spn/spn.js
+++ pw-mypubs-ui/src/main/webapp/mypubs/publication/spn/spn.js
@@ -86,23 +86,7 @@ angular.module('pw.spn', ['pw.lookups'])
   						},
 					browser_spellcheck : true,
 					toolbar : 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | subscript superscript | link | code',
-					valid_elements : "strong/b,i/em,strike,u,"
-							+ "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
-							+ "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
-							+ "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
-							+ "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
-							+ "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
-							+ "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
-							+ "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
-							+ "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face"
-							+ "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
-							+ "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
-							+ "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
-							+ "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
-							+ "valign|width],dfn,fieldset,"
-							+ "kbd,label[for],legend,noscript,"
-							+ "q[cite],samp,small,"
-							+ "tt,var,big"
+					
 				};
 		}
 	]);
